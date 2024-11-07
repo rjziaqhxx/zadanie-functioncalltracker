83e2bec3f0b54cb83e353d514db4b9d1c42f877e
rjziaqhxx <hjcret11@gmail.com>
2024-11-07 20:31:19 +0100

    zad

--- liczba = 0.py
+++ liczba = 0.py
@@ -7,6 +7,10 @@ class FunctionCallTracker:
     def __call__(self):
         return self.funkcja(self)
     
+    def reset(self):
+        self.liczba = 0
+        print(self.liczba)
+    
 @FunctionCallTracker
 def count_calls(self):
     self.liczba += 1
@@ -14,4 +18,7 @@ def count_calls(self):
 
 for i in range(10):
     print(count_calls())
+
+obj = FunctionCallTracker(count_calls())
+obj.reset()
 
